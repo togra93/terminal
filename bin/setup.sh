3c7ecc8243c68ced47172afc95f5786128b73052
Tobias Grau <tobias.grau@itwm.fraunhofer.de>
2017-03-27 16:15:33 +0200

    setting everything up

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,14 +1,39 @@
 # author: togra93
-# last updated: 2017-03-15
+# last updated: 2017-03-27
 # configure shell, using given files
 # basically backup old ones and create symlinks
 
-# link bash related files (.bashrc .bash_aliases .bash_functions)
+vBINDIR=~/bin
+vGITDIR=~/git
+# set true, if you want to check and even clone the liquidprompt setup
+vLIQPROM=false
+vLIQPROMLINK="https://github.com/nojhan/liquidprompt.git"
 
-# link vimrc
+# link dotfiles 
+shopt -s dotglob
+for i in ../dotfiles/*;do
+    vORIGFILE=~/$(basename $i)
+    [ -e $vORIGFILE ] && mv $vORIGFILE $vORIGFILE.old
+    ln -s $i ~/
+done
+shopt -u dotglob
 
 # make bin folder and link binaries
+[ ! -d $vBINDIR ] && mkdir $vBINDIR
+for i in ../bin/*;do
+    ln -s $i $vBINDIR
 
 # optional: clone liquidprompt and link config
+[ $vLIQPROM ] && {
+    vLIQPROMORIG=~/.liquidpromptrc
+    if [ -e $vLIQPROMORIG ];then
+        mv $vLIQPROMPORIG $vLIQPROMORIG.old
+    else
+        [ ! -d $vGITDIR ] && mkdir $vGITDIR
+        git clone -q $vLIQPROMLINK $vGITDIR
+    fi
+    ln -s ../liquidprompt/.liquidpromptrc ~/.liquidpromptrc
+}
 
 # finally source .bashrc
+. ~/.bashrc
