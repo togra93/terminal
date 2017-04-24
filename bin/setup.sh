38867ca50515b8a317aa3573fd48202a25a66177
Tobias Grau <togra93@posteo.de>
2017-04-24 16:19:24 +0200

    ready for testing

--- bin/setup.sh
+++ bin/setup.sh
@@ -11,20 +11,8 @@ vLIQPROM=false
 vLIQPROMLINK="https://github.com/nojhan/liquidprompt.git"
 vTERMINATOR=false
 
-# query options liquidprompt/terminator
-vOPTS='lth'
-if [ ! -z $1 ]; then
-    while getopts "$vOPTS" opt;do
-        case ${opt} in
-            l ) vLIQPROM=true;;
-            t ) vTERMINATOR=true;;
-            * ) help();;
-        esac
-    done
-fi
-
 # help
-help ()
+displayhelp ()
 {
 cat<<EOF
 Configure Shell/Terminal
@@ -39,20 +27,32 @@ EOF
 exit
 }
 
+# query options liquidprompt/terminator
+vOPTS="lth"
+if [ ! -z $1 ];then
+    while getopts "$vOPTS" opt;do
+    {
+        case ${opt} in
+            l ) vLIQPROM=true;;
+            t ) vTERMINATOR=true;;
+            * ) displayhelp;exit;;
+        esac
+    }
+    done 
+fi
+
 # information
 clear
 echo "--------------------------------------"
 echo "--- Setting up your Shell/Terminal ---"
 echo "--------------------------------------"
+echo "To display additional options, use -h"
 echo -e "\nAlready existing files will be moved to <FILE>.bak."
-read -p "Do you want to run this script (y/n)?" answer
+read -p "Do you want to run this script (y/n)? " answer
 case ${answer:0:1} in
-    y|Y )
-        echo "Starting Script ...";;
-    n|N )
-        echo "Aborting ...";exit;;
-    * )
-        echo "Input not valid ...";exit;;
+    y|Y ) echo -e "Starting Script ...\n";;
+    n|N ) echo "Aborting ...";exit;;
+    * )   echo "Input not valid ...";exit;;
 esac
 
 # sync dotfiles
@@ -66,7 +66,7 @@ rsync -rb --suffix=".bak" --exclude="$(basename $0)" $vBASEDIR/bin/* $vBINDIR &&
 # check terminator config
 # need to write function to query installed software on multiple distros first
 ($vTERMINATOR) && {
-
+    :
 }
 
 # clone/setup liquidprompt
@@ -94,4 +94,4 @@ rsync -rb --suffix=".bak" --exclude="$(basename $0)" $vBASEDIR/bin/* $vBINDIR &&
 
 
 echo "--- Setting up your Terminal is now finished."
-echo -e "--- Please run the following command once \e[32msource ~/.bashrc\e[0m"
+echo -e "--- Please run the following command once \e[32msource ~/.bashrc\e[0m\n"
